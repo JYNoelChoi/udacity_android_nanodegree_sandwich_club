4441c797f8e2b76e1dd7a97666fe8f45033616d2
Jun Young <noelchoi@hotmail.com>
2019-08-04 23:40:42 +0900

    initial layout

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,16 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import org.json.JSONException;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+
+    private Sandwich sandwich = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +41,13 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+
+        try {
+            sandwich = JsonUtils.parseSandwichJson(json);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
@@ -57,6 +68,17 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI() {
+        TextView placeOfOrigin = findViewById(R.id.origin_tv);
+        TextView alsoKnownAs = findViewById(R.id.also_known_as_tv);
+        TextView ingredients = findViewById(R.id.ingredients_tv);
+        TextView description = findViewById(R.id.description_tv);
+
+        if (sandwich != null) {
+            placeOfOrigin.setText(sandwich.getPlaceOfOrigin());
+            alsoKnownAs.setText(sandwich.getAlsoKnownAs().toString());
+            ingredients.setText(sandwich.getIngredients().toString());
+            description.setText(sandwich.getDescription());
+        }
 
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,84 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+    public static Sandwich parseSandwichJson(String json) throws JSONException {
+        // Parse Json object
+        JSONObject jsonParsed = null;
+        Sandwich sandwich = new Sandwich();
+        try {
+            jsonParsed = new JSONObject(json);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        // get mainName
+        String mainName = jsonParsed.getJSONObject("name").getString("mainName");
+
+        // get alsoKnownAs
+        List<String> alsoKnownAsArray = new ArrayList<>();
+        JSONArray alsoKnownAsJsonArray = jsonParsed.getJSONObject("name").getJSONArray("alsoKnownAs");
+        for (int i = 0; i < alsoKnownAsJsonArray.length(); i++) {
+            alsoKnownAsArray.add(alsoKnownAsJsonArray.getString(i));
+        }
+
+        // get PlaceOfOrigin
+        String placeOfOrigin = jsonParsed.getString("placeOfOrigin");
+
+        // get Description
+        String description = jsonParsed.getString("description");
+
+        // get ImageUrl
+        String imageUrl = jsonParsed.getString("image");
+
+        // get Ingredients
+        List<String> ingredientsArray = new ArrayList<>();
+        JSONArray ingredientsJsonArray = jsonParsed.getJSONArray("ingredients");
+        for (int j = 0; j < ingredientsJsonArray.length(); j++) {
+            ingredientsArray.add(ingredientsJsonArray.getString(j));
+        }
+
+        // set values to Sandwich Object
+        sandwich.setMainName(mainName);
+        sandwich.setAlsoKnownAs(alsoKnownAsArray);
+        sandwich.setPlaceOfOrigin(placeOfOrigin);
+        sandwich.setDescription(description);
+        sandwich.setImage(imageUrl);
+        sandwich.setIngredients(ingredientsArray);
+
+        return sandwich;
     }
 }
+
+
+/*
+{
+    \"name\":
+        {
+            \"mainName\":\"Ham and cheese sandwich\",
+            \"alsoKnownAs\":[]
+        },
+    \"placeOfOrigin\":\"\",
+    \"description\":\"A ham and cheese sandwich is a common type of sandwich. It is made by putting cheese and sliced ham between two slices of bread. The bread is sometimes buttered and/or toasted. Vegetables
+                like lettuce, tomato, onion or pickle slices can also be included. Various kinds of mustard and mayonnaise are also common.\",
+    \"image\":\"https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Grilled_ham_and_cheese_014.JPG/800px-Grilled_ham_and_cheese_014.JPG\",
+    \"ingredients\":[\"Sliced bread\",\"Cheese\",\"Ham\"]}
+ */
+
+/*
+this.mainName = mainName;
+        this.alsoKnownAs = alsoKnownAs;
+        this.placeOfOrigin = placeOfOrigin;
+        this.description = description;
+        this.image = image;
+        this.ingredients = ingredients;
+ */
